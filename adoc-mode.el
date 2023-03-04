1f741e5f17b2f0b374a5fd31ba13fd5b8424cd57
Максим Дунаевский <mdunaevskii@astralinux.ru>
2023-03-04 20:46:52 +0300

    Fixed commentary typing error

--- adoc-mode.el
+++ adoc-mode.el
@@ -34,8 +34,8 @@
 ;; writing short documents, articles, books and UNIX man pages.  AsciiDoc files
 ;; can be translated to HTML and DocBook markups.
 ;;
-;; `adoc-mode' is an Emacs major mode for editing AsciiDoc files.  It emphasizes on
-;; the idea that the document is highlighted so it pretty much ;; looks like the
+;; adoc-mode is an Emacs major mode for editing AsciiDoc files.  It emphasizes on
+;; the idea that the document is highlighted so it pretty much looks like the
 ;; final output.  What must be bold is bold, what must be italic is italic etc.
 ;; Meta characters are naturally still visible, but in a faint way, so they can
 ;; be easily ignored.
