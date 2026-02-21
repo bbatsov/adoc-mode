48abf5033703110b94cfaf728aaf468f64ec5871
Bozhidar Batsov <bozhidar@batsov.dev>
2026-02-21 10:29:01 +0200

    Extract image display code into adoc-mode-image.el
    The image subsystem (overlays, remote fetching, context menus) is
    self-contained, so move it to its own file to reduce the size of the
    main file. Image tests move to test/adoc-mode-image-test.el.

--- test/adoc-mode-test.el
+++ test/adoc-mode-test.el
@@ -500,59 +500,6 @@ Don't use it for anything real.")
                   "]" adoc-meta-face "\n" nil
                   ))
 
-(ert-deftest adoctest-test-images ()
-  (adoctest-faces "images"
-                  ;; block macros
-                  ;; empty arglist
-                  "image" adoc-complex-replacement-face "::" adoc-meta-face
-                  "./foo/bar.png" adoc-internal-reference-face
-                  "[]" adoc-meta-face "\n" nil
-                  ;; pos attribute 0 = alternate text
-                  "image" adoc-complex-replacement-face "::" adoc-meta-face
-                  "./foo/bar.png" adoc-internal-reference-face
-                  "[" adoc-meta-face "lorem ipsum" adoc-secondary-text-face "]" adoc-meta-face "\n" nil
-                  ;; keyword title
-                  "image" adoc-complex-replacement-face "::" adoc-meta-face
-                  "./foo/bar.png" adoc-internal-reference-face
-                  "[" adoc-meta-face "alt" adoc-attribute-face "=" adoc-meta-face "lorem ipsum" adoc-secondary-text-face "]" adoc-meta-face "\n" nil
-                  ;; keyword alt and title
-                  "image" adoc-complex-replacement-face "::" adoc-meta-face
-                  "./foo/bar.png" adoc-internal-reference-face
-                  "[" adoc-meta-face "alt" adoc-attribute-face "=" adoc-meta-face "lorem ipsum" adoc-secondary-text-face "," adoc-meta-face
-                  "title" adoc-attribute-face "=" adoc-meta-face "lorem ipsum" adoc-secondary-text-face "]" adoc-meta-face "\n" nil
-                  ;; multiline alt and title
-                  "image" adoc-complex-replacement-face "::" adoc-meta-face
-                  "./foo/bar.png" adoc-internal-reference-face
-                  "[" adoc-meta-face "alt" adoc-attribute-face "=" adoc-meta-face
-                  "lorem\nipsum\nsit" adoc-secondary-text-face "," adoc-meta-face
-                  "title" adoc-attribute-face "=" adoc-meta-face
-                  "lorem\nipsum\nsit" adoc-secondary-text-face "]" adoc-meta-face "\n" nil
-
-                  ;; no everything again with inline macros
-                  "foo " 'no-face "image" adoc-complex-replacement-face ":" adoc-meta-face
-                  "./foo/bar.png" adoc-internal-reference-face
-                  "[]" adoc-meta-face "bar" 'no-face "\n" nil
-
-                  "foo " 'no-face "image" adoc-complex-replacement-face ":" adoc-meta-face
-                  "./foo/bar.png" adoc-internal-reference-face
-                  "[" adoc-meta-face "lorem ipsum" adoc-secondary-text-face "]" adoc-meta-face "bar" 'no-face "\n" nil
-
-                  "foo " 'no-face "image" adoc-complex-replacement-face ":" adoc-meta-face
-                  "./foo/bar.png" adoc-internal-reference-face
-                  "[" adoc-meta-face "alt" adoc-attribute-face "=" adoc-meta-face "lorem ipsum" adoc-secondary-text-face "]" adoc-meta-face "bar" 'no-face "\n" nil
-
-                  "foo " 'no-face "image" adoc-complex-replacement-face ":" adoc-meta-face
-                  "./foo/bar.png" adoc-internal-reference-face
-                  "[" adoc-meta-face "alt" adoc-attribute-face "=" adoc-meta-face "lorem ipsum" adoc-secondary-text-face "," adoc-meta-face
-                  "title" adoc-attribute-face "=" adoc-meta-face "lorem ipsum" adoc-secondary-text-face "]" adoc-meta-face "bar" 'no-face "\n" nil
-
-                  "image" adoc-complex-replacement-face ":" adoc-meta-face
-                  "./foo/bar.png" adoc-internal-reference-face
-                  "[" adoc-meta-face "alt" adoc-attribute-face "=" adoc-meta-face
-                  "lorem\nipsum\nsit" adoc-secondary-text-face "," adoc-meta-face
-                  "title" adoc-attribute-face "=" adoc-meta-face
-                  "lorem\nipsum\nsit" adoc-secondary-text-face "]" adoc-meta-face "\n" nil))
-
 (ert-deftest adoctest-test-attribute-list ()
   (adoctest-faces "attribute-list"
                   ;; positional attribute
@@ -1210,5 +1157,7 @@ Don't use it for anything real.")
   ;; todo: execute tests in an smart order: the basic/simple tests first, so
   ;; when a complicated test fails one knows that the simple things do work
   (ert-run-tests-interactively "^adoctest-test-"))
+
+(provide 'adoc-mode-test)
 
 ;;; adoc-mode-test.el ends here
